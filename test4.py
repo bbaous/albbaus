7d2cfed7044f72cd1056f33cd926c9d846c40bab
root <bbaous@gmail.com>
2021-03-13 11:09:03 +0000

    Changes to the variable names

--- test4.py
+++ test4.py
@@ -1,8 +1,8 @@
 from random import seed,random,randint,randrange,sample
-LotteryTickets=10000
-nummberOfLotteries=1
+LotteryTickets=10
+numberOfLotteries=1
 LotterTicketsPicked=10
-LotterDraws=100000
+LotterDraws=10
 
 def randomGenerator2(rangeNumbers, returnNumbers):
      return [randrange(1, rangeNumbers, 1) for i in range(returnNumbers)]
@@ -10,11 +10,11 @@ def randomGenerator2(rangeNumbers, returnNumbers):
 def draw(lotteryDaw,MyLottery):
 	#print ("The lottery draw : " +  str(lotteryDaw))
 	#print ("Your lottery is   : " +  str(MyLottery))
-	if MyLottery[nummberOfLotteries-1] in lotteryDaw:
+	if MyLottery[numberOfLotteries-1] in lotteryDaw:
 		print (True)
 	else:
 		print (False)
  
 for draws in range(1,LotterDraws,1):
-	print ("Draw number %d" %draws)
-	draw(randomGenerator2(LotteryTickets, LotterTicketsPicked),randomGenerator2(LotteryTickets, nummberOfLotteries))
+	print ("Draw number %d" %(draws+1))
+	draw(randomGenerator2(LotteryTickets, LotterTicketsPicked),randomGenerator2(LotteryTickets, numberOfLotteries))
