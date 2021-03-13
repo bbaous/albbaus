27153c665f9e1307c0350e6480fd311b756bce9b
root <bbaous@gmail.com>
2021-03-13 11:02:09 +0000

    Exercice 4 and some variations

--- test4.py
+++ test4.py
@@ -1,40 +1,20 @@
-#Your task is to use the random library to help simulate a lottery competition.
-#In this competition there are 10,000 tickets, each with a unique ID number. Every week there is one 
-#draw, and there are 10 winners (ie. 10 winning numbers are picked).
-#Each ticket is identified by a unique integer number between 1 and 10,000. In this lottery, a ticket 
-#CAN win twice (unusually!), i.e. we replace a ticket back in the draw after it has been pulled out.
+from random import seed,random,randint,randrange,sample
+LotteryTickets=10000
+nummberOfLotteries=1
+LotterTicketsPicked=10
+LotterDraws=100000
 
-#a) Write a function which takes as a parameter a ticket ID number, and simulates a lottery 
-#draw by picking 10 random winners, and returns True if the specified ticket is a winner, 
-#and False otherwise (True and False are Boolean datatypes in Python)
-#b) Use this function to simulate a million (ie. 1,000,000) lottery draws. Assume each time 
-#a different ticket is purchased. How many times did you win in this simulation?
-#c) Discuss the following two points:
-#i. Do you think it makes a difference if the person buys a different ticket 
-#number each week, or if they keep the same number for the million draws?
-#(use your simulation to provide evidence to your answer if you can)
-#ii. How could you improve your answer to part (b) to give you more 
-#confidence in the amount of wins you would expect after a million draws?
-	
-# seed the pseudorandom number generator
-from random import seed,random,randint,randrange
+def randomGenerator2(rangeNumbers, returnNumbers):
+     return [randrange(1, rangeNumbers, 1) for i in range(returnNumbers)]
 
-def matching(rangeNumbers, returnNumbers, matchingLottery):
-	randomGenerator(1000, 10,)
-	return 
-
-def randomGenerator(rangeNumbers, returnNumbers):
-    seed(1)
-    for lottery in range(returnNumbers):
-	    #value = randrange(10000)
-	    #print(value)
-	    #value = randint(0, rangeNumbers)
-	    print('The winning lottery %d is %d' %(lottery+1, randint(0, rangeNumbers)))
-		returnValues=['']
-		returnValues.append(randint(0, rangeNumbers)) 
-	    print(returnValues)
-    return 0
-
-
-
-randomGenerator(1000, 10,)
+def draw(lotteryDaw,MyLottery):
+	#print ("The lottery draw : " +  str(lotteryDaw))
+	#print ("Your lottery is   : " +  str(MyLottery))
+	if MyLottery[nummberOfLotteries-1] in lotteryDaw:
+		print (True)
+	else:
+		print (False)
+ 
+for draws in range(1,LotterDraws,1):
+	print ("Draw number %d" %draws)
+	draw(randomGenerator2(LotteryTickets, LotterTicketsPicked),randomGenerator2(LotteryTickets, nummberOfLotteries))
---
+++ test4a.py
@@ -0,0 +1,50 @@
+#Your task is to use the random library to help simulate a lottery competition.
+#In this competition there are 10,000 tickets, each with a unique ID number. Every week there is one 
+#draw, and there are 10 winners (ie. 10 winning numbers are picked).
+#Each ticket is identified by a unique integer number between 1 and 10,000. In this lottery, a ticket 
+#CAN win twice (unusually!), i.e. we replace a ticket back in the draw after it has been pulled out.
+
+#a) Write a function which takes as a parameter a ticket ID number, and simulates a lottery 
+#draw by picking 10 random winners, and returns True if the specified ticket is a winner, 
+#and False otherwise (True and False are Boolean datatypes in Python)
+#b) Use this function to simulate a million (ie. 1,000,000) lottery draws. Assume each time 
+#a different ticket is purchased. How many times did you win in this simulation?
+#c) Discuss the following two points:
+#i. Do you think it makes a difference if the person buys a different ticket 
+#number each week, or if they keep the same number for the million draws?
+#(use your simulation to provide evidence to your answer if you can)
+#ii. How could you improve your answer to part (b) to give you more 
+#confidence in the amount of wins you would expect after a million draws?
+	
+# seed the pseudorandom number generator
+from random import seed,random,randint,randrange,sample
+
+def matching(rangeNumbers, returnNumbers, matchingLottery):
+	randomGenerator(1000, 10,)
+	return 
+
+def randomGenerator(rangeNumbers, returnNumbers):
+    seed(1)
+    for lottery in range(returnNumbers):
+
+	    #value = randrange(10000)
+	    #print(value)
+	    value = randint(0, rangeNumbers)
+	    print('The winning lottery %d is %d' %(lottery+1, randint(0, rangeNumbers)))
+    return 0
+
+
+def randomGenerator2(rangeNumbers, returnNumbers):
+     return [randrange(1, rangeNumbers, 1) for i in range(returnNumbers)]
+
+
+lotteryDaw=randomGenerator2(20, 10)
+MyLottery=randomGenerator2(10, 1)
+
+print ("The lottery draw : " +  str(lotteryDaw))
+print ("Your number  is  : " +  str(MyLottery))
+if MyLottery[0] in lotteryDaw:
+	print ("You won")
+else:
+	print ("Try again")
+
