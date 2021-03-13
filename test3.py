623b19081eb4c3289ed141ce615b0a4d8db6149f
bbaous <bbaous@gmail.com>
2021-03-13 09:09:21 +0000

    Added comments

--- test3.py
+++ test3.py
@@ -3,7 +3,9 @@ print ("Fibonacci sequence:")
 n1, n2 = 0, 1
 count = 0
 nth=0
-
+stopCondition=1000
+#Perform a while true loop until my main variable n1 becomes 
+# greater than the stopCondition and print the last Fibonacci number and 
 while True:
     print(n1)
     nth = n1 + n2
@@ -11,6 +13,6 @@ while True:
     n1 = n2
     n2 = nth
     count += 1
-    if n1>1000:
+    if n1>stopCondition:
        print(n1)
        break
