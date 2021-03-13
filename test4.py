d389ce302c083885adb1147dc0149048818c8a6d
root <bbaous@gmail.com>
2021-03-13 11:36:44 +0000

    Comments added

--- test4.py
+++ test4.py
@@ -10,14 +10,15 @@ LotteryDraws=100
 #A random regerator returning  pseudorandom set numbers within a specific range  
 def randomGenerator(rangeNumbers, returnNumbers):
      return [randrange(1, rangeNumbers, 1) for i in range(returnNumbers)]
-
+#Compare my ticket with the drawn winning lotteries
 def draw(lotteryDaw,MyLottery):
 	print ("The lottery draw : " +  str(lotteryDaw))
 	print ("Your lottery is   : " +  str(MyLottery))
 	if MyLottery[NumberOfLotteries-1] in lotteryDaw:
 		return True
 	return False
- 
+#Select my random ticket, draw the winning lotteries for "LotterDraws" times and compare
+#the results
 for draws in range(0,LotteryDraws,1):
 	print ("Draw number %d and result is %s " \
 		%((draws+1),draw(randomGenerator(LotteryTickets, LotteryTicketsPicked),\
