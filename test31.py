69ba500d16acd9202f43a8d9a1220c2e6ecae3c1
bbaous <bbaous@gmail.com>
2021-03-13 08:49:45 +0000

    Split exercise 3 to  two versions

--- test31.py
+++ test31.py
@@ -1,16 +1,14 @@
-print ("Fibonacci sequence:")
+#Similar code
 # first two terms
 n1, n2 = 0, 1
 count = 0
 nth=0
 
-while True:
+print("Fibonacci sequence:")
+while n1<1000:
     print(n1)
     nth = n1 + n2
     # update values
     n1 = n2
     n2 = nth
     count += 1
-    if n1>1000:
-       print(n1)
-       break
