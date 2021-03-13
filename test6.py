1a65ce16c66614f0208f04ab169499d98fac56ca
root <bbaous@gmail.com>
2021-03-13 21:21:39 +0000

    test6.py almost ready

--- test6.py
+++ test6.py
@@ -9,20 +9,23 @@ tmin= stationData["tmin"]
 del(tmin[0])
 min=min(tmin,key=lambda x:float(x)) 
 print("The min value of minimum temperature is     : " + str(min)) 
-sun=stationData["sun"]
+#TODO -This the only part missing
+##The Sun column is currently in hours. Recalculate this column so the values are in days (assume a day is 24 hours).
+#Re-save this file as a new filename– you may want to use numpy.savetxt, but you don’t have to.*
+print(stationData["sun"])
+##Change date 
 
-year= stationData["yyyy"]
-del(year[0])
-tmax=stationData["tmax"]
-del(tmax[0])
-yearMax=()
+
+#Plot max temperatures of 2017
 print("Max temperatures of year 2017") 
+counter=1
+plt.figure()  
 for index in stationData.values:
     if index[0]==2017.0:
-       yearMax=(index[0], index[2])
        print (index[0], index[2])
-
-
-plt.figure()  
-plt.plot(yearMax, color='red', lw=2, label='Tmax')
-    
+       plt.scatter(counter,float(index[2]),lw=2, linestyle='--', color='red')
+       counter+=1
+plt.xlabel('T')
+plt.ylabel('TMAX')
+plt.title('Maximum Temperatures in 2017')
+plt.show()
