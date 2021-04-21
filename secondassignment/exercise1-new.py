898431e6e695bc06b3c071fd47edd9adaf320e58
root <bbaous@gmail.com>
2021-04-21 21:08:40 +0100

    updated printing both in the plot

--- secondassignment/exercise1-new.py
+++ secondassignment/exercise1-new.py
@@ -1,3 +1,4 @@
+from operator import le
 import numpy as np
 from numpy.core.arrayprint import dtype_is_implied
 from scipy.integrate import odeint
@@ -7,8 +8,7 @@ import matplotlib.pyplot as plt
 #where X(t) is the population size, and r and K are the model parameters.
 
 def model(x,t):
-    dxdt = r*x*(1-x/K)
-    return dxdt
+    return  r*x*(1-x/K)
 
 # initial conditions
 x0,r,K,dt,tmax=1,1,100,0.5,10
@@ -17,34 +17,30 @@ t = np.linspace(0,tmax,num=int(tmax/dt))
 print(t)
 # solve ODE
 x = odeint(model,x0,t)
-
+print (x)
 # plot results
-
-plt.plot(t,x)
+plt.figure()
+plt.plot(t,x,'r',label='Using for odenint')
 plt.xlabel('time')
 plt.ylabel('x(t)')
-plt.legend(['dt=0.5'], loc='lower right')
-plt.show()
+plt.legend(['Using odenit & dt=0.5'], loc='lower right')
 
 
 x1 = np.zeros(int(tmax/dt))
 i=0
-print (tmax)
-time=0
-while time<tmax:
-    print ("Time is  :"+str(time))
+for j in t:
+  
     if i==0:
         x1[i]=x0
     else:
-        x1[i]=x1[i-1]+model(x1[i-1],time)
-    print ("i is  :"+str(i))
-    time=time+dt
+        x1[i]=x1[i-1]+model(x1[i-1],j)
+    print ("Time is  : "+str(j)+" i  is : "+str(i)+"  x is : "+str(x1[i]))
     i=i+1
 
 # plot results
 
-plt.plot(t,x1)
+plt.plot(t,x1,'b',label='Using for loop')
 plt.xlabel('time')
 plt.ylabel('x(t)')
-plt.legend(['dt=0.5'], loc='lower right')
+plt.legend(['Using odenit dt=0.5','Using for loop dt=0.5'],loc='lower right')
 plt.show()
